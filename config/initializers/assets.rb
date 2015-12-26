f1a5dd0e2b407b91f70d7f8b89522116437da7cf
Bruno Zatta <bzatta@gmail.com>
2015-12-26 10:39:28 -0500

    Add precompile assets and images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,14 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jumbotron.gif )
+Rails.application.config.assets.precompile += %w( full-size-logo-black.png )
+Rails.application.config.assets.precompile += %w( full-size-logo-banner.png )
+Rails.application.config.assets.precompile += %w( hanging-poster.png )
+Rails.application.config.assets.precompile += %w( blog.png )
+Rails.application.config.assets.precompile += %w( twitter-hex.png )
+Rails.application.config.assets.precompile += %w( linkedin-hex.png )
+Rails.application.config.assets.precompile += %w( github-hex.png )
+# Enable the asset pipeline
+Rails.application.config.assets.enabled = true
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
