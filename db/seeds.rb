7ea623fbaff3f8af7346daa49700553e2ef82895
Bruno Zatta <bzatta@gmail.com>
2015-12-30 20:47:40 -0500

    Remove admin from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-admin = User.create(username: "Brunation11", password: "Maingear1")
