177f16bebe63b2db673d1372819c2a595b40a429
Bruno Zatta <bzatta@gmail.com>
2019-12-28 20:11:41 -0500

    refactor App component and stylesheet

--- v2/src/App.js
+++ v2/src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import logo from './assets/full-size-logo-black.png';
+import banner from './assets/jumbotron.gif';
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navItems: [
+        {
+          url: 'foo/.',
+          label: 'Projects',
+          active: true,
+        },
+        {
+          url: 'foo/.',
+          label: 'Blog',
+          active: false,
+        },
+        {
+          url: 'foo/.',
+          label: 'About',
+          active: false,
+        },
+        {
+          url: 'foo/.',
+          label: 'Contact',
+          active: false,
+        }
+      ]
+    }
+  }
+
+  render() {
+    let navItems = this.state.navItems.map((item, index) => {
+      return (
+        <button key={index} className="nav-item">{item.label}</button>
+      )
+    });
+
+    return (
+      <div className="App">
+        <header className="header">
+          {navItems}
+          <img src={logo} className="logo" alt="logo" />
+        </header>
+        <img src={banner} className="banner" alt="banner" />
+        <p className="quote">
+          “CREATIVITY CAN SOLVE ALMOST ANY PROBLEM. THE CREATIVE ACT, THE DEFEAT OF HABIT BY ORIGINALITY, OVERCOMES EVERYTHING.”―GEORGE LOIS
+        </p>
+      </div>
+    );
+  }
+}
